449d4d43a44ab258da6e3a0776ed20620949b9d5
0xThamer <thamertx0@gmail.com>
2018-04-20 09:22:19 +0300

    add views index hello

--- coretabs/urls.py
+++ coretabs/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from . import views
 urlpatterns = [
+	path('', views.index),
+	path('hello/<str:name>/', views.hello),
     path('admin/', admin.site.urls),
 ]
---
+++ coretabs/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse('Hi there, this is our new home page.')
+
+def hello(request, name):
+	return HttpResponse('Hello {}'.format(name))
