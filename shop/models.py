a7678eeea4e81d69be7829d4b4685e80b0224dcd
0xThamer <thamertx0@gmail.com>
2018-04-27 19:44:03 +0300

    Django ORM

--- shop/models.py
+++ shop/models.py
@@ -1,20 +1,38 @@
 from django.db import models
 
+
 # Create your models here.
+from django.template.defaultfilters import slugify
+
+
 class Category(models.Model):
-	name = models.CharField(max_length=100)
-	slug = models.SlugField(unique=True, blank=True)
-	created_at = models.DateTimeField(auto_now_add=True)
-	class Mate:
- 		ordering = ('name', )
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Mate:
+        ordering = ('name',)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
 
 class Product(models.Model):
-	name  = models.CharField(max_length=100)
-	price = models.DecimalField(max_digits=10, decimal_places=2)
-	stock = models.PositiveIntegerField()
-	created_at = models.DateTimeField(auto_now_add=True)
-	description = models.TextField(blank=True)
-	slug = models.SlugField(unique=True, blank=True)
-	category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-	class Mate:
-		ordering = ('name', )
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True)
+    slug = models.SlugField(unique=True, blank=True)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+
+    class Mate:
+        ordering = ('name',)
+
+    def save(self, *args, **Kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+
+        super().save(*args, **Kwargs)
